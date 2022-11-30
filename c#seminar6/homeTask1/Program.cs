6481b3ee529b8831e09b207b8ed2a33599c48a24
Pavel <pavelatavin@yandex.ru>
2022-11-30 19:06:40 +0500

    Seminar6Homework

---
+++ c#seminar6/homeTask1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("какое количествоцифр планируется к вводу?");
+
+int numberOfnumbers = int.Parse(Console.ReadLine());
+int[] array = new int [numberOfnumbers];
+
+void NumberOfPositiveNumbers (int[] Array) 
+{
+    int sum =0;
+    for (int i=0;i<Array.Length; i++)
+    {
+        Console.WriteLine ("Введите число");
+        Array[i]= int.Parse(Console.ReadLine());
+        if (Array[i]>0) sum++;
+    }
+    Console.WriteLine($"всего введено {sum} чисел больше 0");
+}
+
+NumberOfPositiveNumbers(array);
---
+++ c#seminar6/homeTask2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+int RememberUserInput(string text)
+{
+    int input =int.Parse(Console.ReadLine());
+    return input;
+}
+
+int a1 = RememberUserInput("Ведите первую координату первой прямой a1:");
+int a2 = RememberUserInput("Ведите вторую координату первой прямой a2:");
+int b1 = RememberUserInput("Ведите первую координату второй прямой b1:");
+int b2 = RememberUserInput("Ведите вторую координату второй прямой b2:");
+
+void CrossPoint (int a1,int a2,int b1,int b2)
+{
+int x = (b2-b1)/(a1-a2);
+int y = (a1*(b2-b1)/(a1-a2)+b1);
+Console.WriteLine ($"точка пересечения прямых x= {x} и y= {y}");
+}
+CrossPoint (a1,a2,b1,b2);
