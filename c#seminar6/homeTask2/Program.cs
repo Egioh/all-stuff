28b77bec410c79618a328d75bfbf87a89cf5c6ef
Pavel <pavelatavin@yandex.ru>
2022-11-30 20:29:21 +0500

    homeWorkV2.0

--- c#seminar6/homeTask2/Program.cs
+++ c#seminar6/homeTask2/Program.cs
@@ -3,21 +3,22 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-int RememberUserInput(string text)
+double RememberUserInput(string text)
 {
-    int input =int.Parse(Console.ReadLine());
+    Console.WriteLine (text);
+    double input = Convert.ToDouble(Console.ReadLine());
     return input;
 }
 
-int a1 = RememberUserInput("Ведите первую координату первой прямой a1:");
-int a2 = RememberUserInput("Ведите вторую координату первой прямой a2:");
-int b1 = RememberUserInput("Ведите первую координату второй прямой b1:");
-int b2 = RememberUserInput("Ведите вторую координату второй прямой b2:");
+double b1 = RememberUserInput("Ведите первую координату первой прямой b1:");
+double k1 = RememberUserInput("Ведите вторую координату первой прямой k1:");
+double b2 = RememberUserInput("Ведите первую координату второй прямой b2:");
+double k2 = RememberUserInput("Ведите вторую координату второй прямой k2:");
 
-void CrossPoint (int a1,int a2,int b1,int b2)
+void CrossPoint (double b1,double b2,double k1,double k2)
 {
-int x = (b2-b1)/(a1-a2);
-int y = (a1*(b2-b1)/(a1-a2)+b1);
+double x = (b2-b1)/(k1-k2);
+double y = (k1*x+b1);
 Console.WriteLine ($"точка пересечения прямых x= {x} и y= {y}");
 }
-CrossPoint (a1,a2,b1,b2);
+CrossPoint (b1,b2,k1,k2);
